b8f232783911afdfe24d1305dea3f869b144fb0b
Irina Eliseeva <madamlindalopez@yandex.ru>
2023-02-22 19:03:47 +0300

    program written

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿//Написать программу которая из имеющегося массива строк формирует массив из строк длина которых меньше, либо равна трём символам
+class Program
+{
+
+    static string[] readStrings()
+    {
+        string originalString = Console.ReadLine() ?? "";
+        return originalString.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+    }
+
+    static void PrintStrings(string[] strings)
+    {
+        for (int i = 0; i < strings.Length; i++)
+            Console.Write(strings[i] + " ");
+        Console.WriteLine();
+    }
+
+    static string[] Filter(string[] strings, int len)
+    {
+        var result = new string[strings.Length];
+        var i = 0;
+        foreach (var s in strings)
+        {
+            if (s.Length <= len)
+            {
+                result[i] = s;
+                i++;
+            }
+        }
+        return result;
+    }
+    static void Main()
+    {
+        Console.WriteLine("Введите желаемую строку для обработки: ");
+        string[] from = readStrings();
+
+        Console.WriteLine("Исходный массив: ");
+        PrintStrings(from);
+        Console.WriteLine("Отфильтрованный массив: ");
+        string[] filtered = Filter(from, 3);
+        PrintStrings(filtered);
+    }
+}
